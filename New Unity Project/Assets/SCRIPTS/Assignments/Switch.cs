bb0429b08bdbc4e1225bcae020499e2cc8543226
Sam Ahmu <10590652@my.uvu.edu>
2019-02-27 17:07:06 -0700

    Created Switch Script

---
+++ New Unity Project/Assets/SCRIPTS/Assignments/Switch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+	public string suspect;
+
+	public string weapon;
+
+	public string room;
+
+
+	// Use this for initialization
+	void Start () {
+		MurderMystery(suspect);
+
+		
+	}
+	
+
+	void MurderMystery (string person){
+
+		switch(person){
+
+			case "Mr. Ketchup":
+				print("I was in the billiard room, smoking a cigar and playing pool with with Mr. Radish");
+			break;
+			case "Mr. Radish":
+				print("I was in the billiard room, smoking a cigar and playing pool with with Mr. Ketchup");
+			break;
+			case "Ms. Frysauce":
+				print("I was talking on the rotary phone with my mother, Mrs. Mayo");
+			break;
+			case "Mrs. Mayo":
+				print("I was in the kitchen cleaning up the dishes");
+			break;
+			default:
+				print("I am not familiar with "+suspect+"!");
+			break;
+		}
+
+	}
+}
