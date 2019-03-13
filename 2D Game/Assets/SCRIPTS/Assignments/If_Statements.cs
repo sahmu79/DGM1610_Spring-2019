43865e22db47b2f385b3b7a5fed5e69286de60c0
Sam Ahmu <10590652@my.uvu.edu>
2019-03-13 17:20:11 -0600

    Fixed issues, Created Main Menu

---
+++ 2D Game/Assets/SCRIPTS/Assignments/If_Statements.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class If_Statements : MonoBehaviour {
+
+	public string stopLight;
+	public bool isUtahn = true;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(stopLight == "Red" ){
+			
+			if(isUtahn){
+				print("STOP! The light is RED!");	
+			}
+			else{
+				print("Thank you for stopping at the red light!");
+			}
+		}
+		else if(stopLight == "Yellow" ){
+			
+			if(isUtahn){
+				print("You've got this, bruh!");
+			}
+			else{
+				print("Thank you for preparing to stop!");
+			}
+		}
+		else if(stopLight == "Green" ){
+			
+			if(isUtahn){
+				print("Check your phone, turn up radio, sit at light till it turns yellow, then go!");
+			}
+			else{
+				print("Thank you for driving properly!");
+			}
+		}
+	}
+}
---
+++ 2D Game/Assets/SCRIPTS/FirstScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour {
+    
+    public int WaterBottle = 60;
+    
+	// Use this for initialization
+	void Start () {
+		print(WaterBottle);
+        Debug.Log(WaterBottle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
