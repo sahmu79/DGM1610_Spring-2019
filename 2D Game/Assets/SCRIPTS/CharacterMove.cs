1f38995b9913cddcb694a853d46f19d9042ea5e7
Sam Ahmu <10590652@my.uvu.edu>
2019-04-17 17:00:59 -0600

    Worked on PlayerController

--- 2D Game/Assets/SCRIPTS/CharacterMove.cs
+++ 2D Game/Assets/SCRIPTS/CharacterMove.cs
@@ -7,12 +7,17 @@ public class CharacterMove : MonoBehaviour {
 	// Player movement variables
 	public float moveSpeed;
 	public float jumpHeight;
+	private bool doubleJump;
 
 	// Player grounded variables
 	private bool grounded;
 	public Transform groundCheck;
 	public float groundCheckRadius;
 	public LayerMask whatIsGround;
+
+	// Non-Slide Player
+	private float moveVelocity;
+	public Animator animator;
 
     
     
@@ -30,21 +35,46 @@ public class CharacterMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// Moves Player left and right
-		if(Input.GetKey(KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		}
-		else if(Input.GetKey(KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		}
-
-
 		// Make player jump
 		if(Input.GetKeyDown(KeyCode.W) && grounded){
 			Jump();
 
-	
+		// Double Jump Code
+		if(grounded){
+			doubleJump = false;
+			animator.SetBool("isJumping",false);
 		}
+
+		if(Input.GetKeyDown (KeyCode.Space)&& !doubleJump && !grounded){
+			Jump();
+			doubleJump = true;
+		}
+		// Non-Slide Player
+		moveVelocity = 0f;
+		
+
+		// Moves Player left and right
+		if(Input.GetKey(KeyCode.D)){
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = moveSpeed
+			animator.SetBool("isWalking",true);
+		}
+
+		else if(Input.GetKeyUp (KeyCode.D)){
+			animator.SetBool("isWalking",false);
+		}
+
+		if(Input.GetKey(KeyCode.A)){
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = -moveSpeed
+			animator.SetBool("isWalking",true);
+		}
+		
+		else if(Input.GetKeyUp (KeyCode.A)){
+			animator.SetBool("isWalking",false);
+		}
+		GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
 
 		// Player Flip
 		if (GetComponent<Rigidbody2D>().velocity.x > 0)
